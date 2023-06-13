7409fca659b805931e614c28d15fb3a703ca45d4
thiagolealg <113521516+thiagolealg@users.noreply.github.com>
2023-06-13 13:32:59 -0300

    Update unzip_pdb_files_commented.py
    The provided code is designed to assist in the extraction and analysis of data from the RSCB PDB (Protein Data Bank). The PDB is a widely used repository of experimentally determined 3D structures of biological macromolecules, including proteins and nucleic acids.
    
    The code begins by importing the necessary modules: os for operating system-related functions, gzip for handling compressed gzip files, and shutil for file operations.
    
    Next, the code sets the input and output directories. The data_dir variable should be modified to reflect the path to the directory where the RSCB PDB files are located. Similarly, the output_dir variable should be set to the desired location for the unzipped output files.
    
    To ensure that the output directory exists, the code uses the os.makedirs() function with the exist_ok=True argument. This function creates the directory if it does not already exist or does nothing if it does.
    
    The code then searches for PDB files in the input directory using the os.listdir() function. It filters the files based on the condition that they end with the extension ".ent.gz". The resulting list of PDB files is stored in the pdb_files variable.
    
    Next, the code enters a loop to process each PDB file. For each file, it performs the following steps:
    
    Opens the gzip file using gzip.open() with the full path of the file.
    Opens a new file in write binary mode using the open() function with the corresponding file name in the output directory. The ".gz" extension is removed from the file name by using pdb_file[:-3].
    Copies the contents from the input gzip file to the output file using the shutil.copyfileobj() function. This function efficiently copies data from one file-like object (f_in) to another (f_out).

--- python/unzip_pdb_files_commented_1.py
+++ python/unzip_pdb_files_commented_1.py
@@ -2,8 +2,21 @@ import os
 import gzip
 import shutil
 
+# Specify the input and output directories
 data_dir = 'C:/Users/thiag/Downloads/all_pdb_data'  # Replace with your path
 output_dir = 'C:/Users/thiag/Downloads/all_pdb_data/unzipped'  # Output directory
-os.makedirs(output_dir, exist_ok=True)  # Create the output directory if it doesn't exist
 
+# Create the output directory if it doesn't exist
+os.makedirs(output_dir, exist_ok=True)
+
+# Get a list of PDB files in the input directory
 pdb_files = [f for f in os.listdir(data_dir) if f.endswith('.ent.gz')]
+
+# Iterate over each PDB file
+for pdb_file in pdb_files:
+    # Open the gzip file for reading
+    with gzip.open(os.path.join(data_dir, pdb_file), 'rb') as f_in:
+        # Open a new file in the output directory for writing (remove .gz extension)
+        with open(os.path.join(output_dir, pdb_file[:-3]), 'wb') as f_out:
+            # Copy the contents from the input file to the output file
+            shutil.copyfileobj(f_in, f_out)
